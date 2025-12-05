dec3c3c8300a3e670c9c70141be099b70d07a62c
Hiroki_ujita <hiroki_ujita@hotmail.com>
2025-12-05 09:35:46 +0900

    問題2

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link, NavLink } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 
-function App(){
-return(
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-    </Routes>
+function App() {
+  return (
+    <div>
+      <nav style={{ marginBottom: 20 }}>
+        <Link to="/">ホーム</Link> |{" "}
+        <NavLink
+          to="/about"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          このサイトについて
+        </NavLink>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
   );
 }
 
